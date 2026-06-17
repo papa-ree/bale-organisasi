6e2d8ad1599a4a9a4068817ef4f943d9ee0187ac
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2026-06-17 15:54:00 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,19 +18,6 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <x-umpak::analytics />
 
-    <script>
-        // This code should be added to <head>.
-        // It's used to prevent page load glitches.
-        const html = document.querySelector( 'html' );
-        const isLightOrAuto = localStorage.getItem( 'hs_theme' ) === 'light' || ( localStorage.getItem( 'hs_theme' ) === 'auto' && !window.matchMedia( '(prefers-color-scheme: dark)' ).matches );
-        const isDarkOrAuto = localStorage.getItem( 'hs_theme' ) === 'dark' || ( localStorage.getItem( 'hs_theme' ) === 'auto' && window.matchMedia( '(prefers-color-scheme: dark)' ).matches );
-
-        if ( isLightOrAuto && html.classList.contains( 'dark' ) ) html.classList.remove( 'dark' );
-        else if ( isDarkOrAuto && html.classList.contains( 'light' ) ) html.classList.remove( 'light' );
-        else if ( isDarkOrAuto && !html.classList.contains( 'dark' ) ) html.classList.add( 'dark' );
-        else if ( isLightOrAuto && !html.classList.contains( 'light' ) ) html.classList.add( 'light' );
-    </script>
-
     @livewireStyles
 </head>
 
