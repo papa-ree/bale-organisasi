db7db8fdb0d4287791a8ea1ebfebf412a3d3c00c
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2026-07-21 12:43:27 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,8 @@
 <header
     class="sticky top-0 z-50 bg-white/95 dark:bg-slate-900/95 backdrop-blur-md border-b border-slate-100 dark:border-slate-800 shadow-sm transition-colors duration-300">
+
+    <x-gupa::honeypot fieldName="website_url" action="{{ route('index') }}" />
+
     <nav x-data="umpakNav()" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
 
